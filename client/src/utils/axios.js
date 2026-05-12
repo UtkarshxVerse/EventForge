1a8381e932240f224a3a638ad754e4a9ef8aa898
UtkarshxVerse <utkarsh2423@gmail.com>
2026-05-12 22:07:35 +0530

    new commit

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://eventforge-backend-6j6u.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
